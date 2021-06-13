126711db6d268b95bf2deaffbb8abe55a51abcdf
Prakhar Singh <singhprakhar973@gmail.com>
2021-06-13 23:13:31 +0530

    login form created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css'
 import { ChatEngine } from 'react-chat-engine'
 import  ChatFeed  from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+
+  if(!localStorage.getItem('username')) return <LoginForm />
   return (
     <div>
       <ChatEngine 
       height="100vh" 
       projectID="63d9ea97-3816-4ac5-9d30-eb6c1ff63100" 
-      userName="Butcher" 
-      userSecret="Pr@khar7"
+      userName= {localStorage.getItem('username')} 
+      userSecret= {localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>} 
       />
 
